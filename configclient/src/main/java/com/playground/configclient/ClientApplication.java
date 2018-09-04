508f4d825f6bb30867998163dda50aae76ea7834
Amit Mittal <amit.mittal1993@gmail.com>
2018-09-04 12:49:37 +0530

    Added an Hystrix command and the fallback. If EurekaServer is not up, client is failing to start

--- configclient/src/main/java/com/playground/configclient/ClientApplication.java
+++ configclient/src/main/java/com/playground/configclient/ClientApplication.java
@@ -3,10 +3,12 @@ package com.playground.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ClientApplication {
 
     public static void main(String[] args) {
--- configclient/src/main/java/com/playground/configclient/controller/TestController.java
+++ configclient/src/main/java/com/playground/configclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.playground.configclient.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,5 +15,15 @@ public class TestController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getLuckyWord() {
         return "The lucky word is " + luckyWord;
+    }
+
+    @RequestMapping(value = "/fail", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
+    public String doFailedCall() {
+        throw new RuntimeException("Fail!!!!");
+    }
+
+    public String fallbackMethod() {
+        return "Don't worry, it's been taken care";
     }
 }
